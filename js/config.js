dfe7eed48f3374357907b2cb9e1ccf2ca60a7a0c
lmh263 <80567015+lmh263@users.noreply.github.com>
2023-12-11 01:28:30 +0800

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,20 @@
+var BaseUrl =  "http://180.188.22.245:1999";
+var ResourceUrl = 'http://180.188.22.245:10001/ttxinshenshou/';
+
+if(window.location.pathname!='/'
+    &&window.location.pathname!='/index.html'
+    &&window.location.pathname!='/site/login.html'
+    &&window.location.pathname!='/site/wxredd.html')
+{
+    var wwid=window.localStorage.getItem("wwid");
+    if(!wwid)
+    {
+        localStorage.setItem('enter_url',encodeURIComponent(window.location.href))
+
+        alert('请先登录');
+        window.location.href = '/index.html';
+    }
+}
+
+
+        
